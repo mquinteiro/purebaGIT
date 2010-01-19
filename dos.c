42aa437d169d3348e28989adeab857383947a926
Manuel Quinteiro <mquinteiro@cic-systems.com>
2010-01-19 18:03:01 +0100

    un cambio que hice en el master despues hice un branck y ahora hago un commit

--- dos.c
+++ dos.c
@@ -4,3 +4,4 @@ añado una linea al fichero dos
 
 añado esta linea a experimental
 añado otra linea mas que se ha hecho despues del tag V1
+añado otra linea mas que se ha hecho despues del tag V1 otra linea mas
