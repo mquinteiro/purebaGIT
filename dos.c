b8851c0b6365a4d792f2eb7127edf11f7f081ba1
Manuel Quinteiro <mquinteiro@cic-systems.com>
2010-01-19 18:06:18 +0100

    otro cambio en mifork

--- dos.c
+++ dos.c
@@ -5,3 +5,4 @@ añado una linea al fichero dos
 añado esta linea a experimental
 añado otra linea mas que se ha hecho despues del tag V1
 añado otra linea mas que se ha hecho despues del tag V1 otra linea mas
+ inserto esta linea despues de haber hecho un merge, ahora vuelve a ser diferente, estoy modificando en mifork
